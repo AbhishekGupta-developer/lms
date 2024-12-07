977daa216a6c81c7e4f2fa3b36686fac063580be
Abhishek Gupta <abhishekg.developer@gmail.com>
2024-12-07 20:32:47 +0530

    Modify ServerController

--- src/main/java/com/ducat/lms/controller/ServerController.java
+++ src/main/java/com/ducat/lms/controller/ServerController.java
@@ -6,12 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
 @RestController
 @RequestMapping("/")
 public class ServerController {
 
     @GetMapping
-    public ResponseEntity<String> checkServerHealth() {
-        return new ResponseEntity<>("Server is running!", HttpStatusCode.valueOf(200));
+    public ResponseEntity<?> checkServerHealth() {
+        ServerStatus serverStatus = new ServerStatus();
+        return new ResponseEntity<>(serverStatus, HttpStatusCode.valueOf(200));
+    }
+
+    class ServerStatus {
+        private LocalDateTime currentDateTime = LocalDateTime.now();
+        private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        public String status = "Server is live!";
+        public String date = currentDateTime.format(dateFormatter);
+        public String time = currentDateTime.format(timeFormatter);
+        public String timeZoneId = TimeZone.getDefault().getID();
     }
 }
